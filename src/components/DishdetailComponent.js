ff8efcc8e8aec9dfdcf65a841bb0d430762d7bcb
Chirag Agarwal <chiragga24@gmail.com>
2021-05-29 16:43:56 +0530

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -33,8 +33,8 @@ class CommentForm extends Component{
     }
 
     handleSubmit(values){
-        console.log("Current State is: " + JSON.stringify(values));
-        alert('Current State is: ' + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render(){
@@ -131,7 +131,7 @@ function RenderDish({dish}) {
             );
         }
     }
-    function RenderComments({comments}){
+    function RenderComments({comments, addComment, dishID}){
         if (comments == null) {
             return (<div></div>)
         }
@@ -156,7 +156,7 @@ function RenderDish({dish}) {
                 <ul className='list-unstyled'>
                     {cmnts}
                 </ul>
-                <CommentForm/>
+                <CommentForm dishId = {dishID} addComment={addComment}/>
             </div>
         )
     }
@@ -184,7 +184,9 @@ function RenderDish({dish}) {
                 </div>
                 <div className='row'>
                     <RenderDish dish = {props.dish}/>
-                    <RenderComments comments = {props.comments} />
+                    <RenderComments comments = {props.comments} 
+                    addComment = {props.addComment}
+                    dishID ={props.dish.id}/>
                 </div>
              </div>
         )
